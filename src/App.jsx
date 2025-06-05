e18d9b18920f28ad163602cc1b401137deeec95f
Massimo1167 <m.centrella@gmail.com>
2025-06-05 18:01:28 +0200

    Modifica nome cartella Anagrafica
    descrizione

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import FoglioAssistenzaFormPage from './pages/FoglioAssistenzaFormPage';
 import FoglioAssistenzaDetailPage from './pages/FoglioAssistenzaDetailPage';
 
 // Componenti anagrafici (presumendo che tu li abbia già)
-import ClientiManager from './components/Anagrafiche/ClientiManager';
-import TecniciManager from './components/Anagrafiche/TecniciManager';
-import CommesseManager from './components/Anagrafiche/CommesseManager';
-import OrdiniClienteManager from './components/Anagrafiche/OrdiniClienteManager';
+import ClientiManager from './components/anagrafiche/ClientiManager';
+import TecniciManager from './components/anagrafiche/TecniciManager';
+import CommesseManager from './components/anagrafiche/CommesseManager';
+import OrdiniClienteManager from './components/anagrafiche/OrdiniClienteManager';
 
 import './App.css'; // Il tuo CSS globale
 
