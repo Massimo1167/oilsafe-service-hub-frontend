7777be9a9e395c109ea24dbc379831491d30b41a
Massimo1167 <m.centrella@gmail.com>
2025-06-05 19:11:20 +0200

    Update supabaseClient.js
    modifica di URL e Key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Sostituisci con i tuoi valori reali!
-const supabaseUrl = 'https://snrorrdmprbrqxdojgtp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNucm9ycmRtcHJicnF4ZG9qZ3RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNDIyNTQsImV4cCI6MjA2MzkxODI1NH0.LPxBdoOEwEQAB4BhHbxudu2eUWg42ewN8085ZL34CMY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error("Errore: URL Supabase o Chiave Anon non definite. Controlla supabaseClient.js e le variabili d'ambiente.");
